81fee81720cb25c28571bfd050e4b22dd50944c0
Kristian Koskimäki <koskimaki@gmail.com>
2021-11-03 20:30:55 +0200

    Simplify handling of incorrect folder names

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,24 +9,18 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
 }
 
 void MainWindow::on_scan_folders_clicked() {
-    QString not_found, typed_folders = ui->folders_box->text().remove(QRegularExpression("\"|^ *| +$|\\*\\.?j?p?e?g?$"));
-    QStringList fixed_folders;                              //remove quotes, leading and trailing space, .jpg extension
+    QStringList fixed_folders;
+    QString not_found, typed_folders = ui->folders_box->text().replace(";:+;", ";");
+    typed_folders.remove(QRegularExpression("\"|^ *| +$|\\*\\.?j?p?e?g?$"));    //remove colon, quotes, extra space, .jpg
 
     for (auto &folder : typed_folders.split(QStringLiteral(";"))) {
-        QFileInfo file = QFileInfo(folder);
-        const QString filename = file.fileName();
-        const QString path = file.absoluteFilePath();
-
-        if (filename == QStringLiteral("") ||                               //"c:" is read as "c:/[a root folder]"
-            filename.at(filename.size()-1) != '/' )                         //"d:" is ok, as is "c:/" (bug?)
-            file = QFileInfo(folder + QStringLiteral("/"));
-        if (folder == QStringLiteral("") || folder == QStringLiteral(":"))
-            continue;
-        if (file.absolutePath() == QDir::currentPath() || !QFileInfo::exists(path)) {
+        if (folder == QStringLiteral("c:") || folder == QStringLiteral("C:"))
+            folder = QStringLiteral("c:\\");                //bug? "c:" -> Qt debug folder, but "d:" -> "d:\"
+        const QString path = QFileInfo(folder).absoluteFilePath();
+        if (!QFileInfo::exists(path)) {
             not_found = not_found + folder + QStringLiteral("  ");
             continue;
         }
-
         bool add_this = true;                                //eliminate duplicate folders in list
         for (int i=fixed_folders.size()-1; i>=0; i--) {
             if (path.indexOf(fixed_folders.at(i)) == 0) {    //duplicate OR parent already added
