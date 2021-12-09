601842bd2bdd03da272e1941060b9874ae1575ff
Kristian Koskimäki <koskimaki@gmail.com>
2021-12-09 22:11:24 +0200

    Delete commented ClickableLabel class

--- mainwindow.h
+++ mainwindow.h
@@ -60,16 +60,5 @@ signals:
 private:
     Ui::MainWindow *ui;
 };
-/*
-class ClickableLabel : public QLabel
-{
-    Q_OBJECT
-public:
-    explicit ClickableLabel(QWidget *parent) { Q_UNUSED (parent) }
-protected:
-    void mousePressEvent(QMouseEvent *event) { Q_UNUSED (event) emit clicked(); }
-signals:
-    void clicked();
-};
-*/
+
 #endif // MAINWINDOW_H
